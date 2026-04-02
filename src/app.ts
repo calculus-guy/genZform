a7823c36537602b1212a0409408909e8ea529cba
calculus guy <sakariyauabdullateef993@gmail.com>
2026-04-02 16:03:03 +0100

    chore: added cors in app

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,10 @@ if (env.nodeEnv !== 'production') {
 }
 
 app.use(helmet());
-app.use(cors({ origin: env.corsOrigin, credentials: true }));
+app.use(cors({
+  origin: ['https://Tf.thegenzhr.com', 'https://talentfactory.netlify.app', 'http://localhost:5000'],
+  credentials: true,
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
